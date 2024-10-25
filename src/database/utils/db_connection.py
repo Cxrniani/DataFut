35f329a7965d20f52b6e0c8963d1ef3151c558df
Matheus Corniani <matheuscorniani@gmail.com>
2024-10-25 15:59:11 -0300

    🚨fix: added right database path

--- src/database/utils/db_connection.py
+++ src/database/utils/db_connection.py
@@ -1,5 +1,5 @@
 import sqlite3
 
 def get_db_connection():
-    conn = sqlite3.connect('path/to/your/database.db')  # Especifique o caminho para o seu banco de dados
+    conn = sqlite3.connect('DataFut\src\database\datafut.db')
     return conn
