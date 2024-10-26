5e09ee96b232d293e3e623780a8c835e46144ac8
Matheus Corniani <matheuscorniani@gmail.com>
2024-10-25 21:08:38 -0300

    🚨fix: fixed connection

--- src/database/services/fixtures.py
+++ src/database/services/fixtures.py
@@ -2,13 +2,19 @@ from ..utils.db_connection import get_db_connection
 from ..utils.data_verification import record_exists
 
 def insert_fixture(id, home_team, away_team, fixture_date, venue_name, venue_city, referee, status):
-    if not record_exists("SELECT 1 FROM fixtures WHERE id = ?", (id,)):
-        conn = get_db_connection()
+    conn = None  
+    try:
+        conn = get_db_connection() 
         cursor = conn.cursor()
-        cursor.execute('''
-            INSERT INTO fixtures (id, home_team, away_team, fixture_date, venue_name, venue_city, referee, status) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
-            (id, home_team, away_team, fixture_date, venue_name, venue_city, referee, status))
-        conn.commit()
-        conn.close()
-    conn.close()
+        
+        if not record_exists("SELECT 1 FROM fixtures WHERE id = ?", (id,)):
+            cursor.execute('''INSERT INTO fixtures (id, home_team, away_team, fixture_date, venue_name, venue_city, referee, status) 
+                              VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
+                           (id, home_team, away_team, fixture_date, venue_name, venue_city, referee, status))
+            conn.commit()
+    except Exception as e:
+        print(f"Erro ao inserir fixture: {e}")  # Tratamento de erro para depuração
+    finally:
+        if conn:  # Verifica se conn foi inicializada antes de fechar
+            conn.close()
+
