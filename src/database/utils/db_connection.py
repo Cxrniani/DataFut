70363d102466528234cd3a73cbae36126dcbd64b
Matheus Corniani <matheuscorniani@gmail.com>
2024-10-29 18:15:42 -0300

    ♻️ refactor: update database connection from local to AWS RDS

--- src/database/utils/db_connection.py
+++ src/database/utils/db_connection.py
@@ -1,5 +1,19 @@
-import sqlite3
+import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
 
 def get_db_connection():
-    conn = sqlite3.connect('DataFut\src\database\datafut.db')
+    conn = pymysql.connect(
+        host= db_host,
+        user= db_user,
+        password= db_password,
+        database= db_name
+    )
     return conn
